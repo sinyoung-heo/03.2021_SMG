5801069e03bbea9eab9259d669fa02076eeee8f9
HSY <hsykpu@gmail.com>
2021-03-22 22:16:05 +0900

    Upload Assignment

---
+++ 00.Assignment/4week/app/src/main/java/kr/ac/kpu2014180044/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package kr.ac.kpu2014180044.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private CheckBox firewallCheckbox;
+    private TextView outTextView;
+    private EditText userEditText;
+    private TextView editTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firewallCheckbox = findViewById(R.id.checkbox);
+        outTextView = findViewById(R.id.outTextView);
+        userEditText = findViewById(R.id.userEditText);
+        userEditText.addTextChangedListener(textWatch);
+        editTextView =findViewById(R.id.editTextView);
+    }
+
+    private TextWatcher textWatch = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            editTextView.setText("String length = " + s.length());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    public void onBtnApply(View view) {
+        boolean checked = firewallCheckbox.isChecked();
+        String text = checked ? "Using Firewall" : "Not using Firewall";
+        outTextView.setText(text);
+
+        String user = userEditText.getText().toString();
+        editTextView.setText("User info = " + user);
+    }
+
+    public void onCheckFirewall(View view) {
+        boolean checked = firewallCheckbox.isChecked();
+        String text = checked ? "Checked Firewall" : "Unchecked Firewall";
+        outTextView.setText(text);
+    }
+}
