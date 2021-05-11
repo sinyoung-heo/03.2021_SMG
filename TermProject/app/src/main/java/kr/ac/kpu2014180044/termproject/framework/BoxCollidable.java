0cc7ebca221ebb9951c1aedcdef52f4078c69e26
HSY <hsykpu@gmail.com>
2021-05-11 19:27:50 +0900

    Add Project

---
+++ TermProject/app/src/main/java/kr/ac/kpu2014180044/termproject/framework/BoxCollidable.java
@@ -0,0 +1,8 @@
+package kr.ac.kpu2014180044.termproject.framework;
+
+import android.graphics.RectF;
+
+public interface BoxCollidable {
+    //public RectF getBoundingRect();
+    public void getBoundingRect(RectF rect);
+}
---
+++ TermProject/app/src/main/java/kr/ac/kpu2014180044/termproject/framework/GameObject.java
@@ -0,0 +1,8 @@
+package kr.ac.kpu2014180044.termproject.framework;
+
+import android.graphics.Canvas;
+
+public interface GameObject {
+    public void update();
+    public void draw(Canvas canvas);
+}
