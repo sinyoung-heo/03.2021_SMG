f9902f2d5a32305734d321a771d07085445ea27d
HSY <hsykpu@gmail.com>
2021-05-11 19:43:33 +0900

    add imagebutton image

--- TermProject/app/src/main/java/kr/ac/kpu2014180044/termproject/MainActivity.java
+++ TermProject/app/src/main/java/kr/ac/kpu2014180044/termproject/MainActivity.java
@@ -3,12 +3,22 @@ package kr.ac.kpu2014180044.termproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton dirChangeButton;
+    private ImageButton upStairButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dirChangeButton = findViewById(R.id.DirChange);
+        upStairButton = findViewById(R.id.UpStairs);
+
+        dirChangeButton.setImageResource(R.mipmap.dir_change);
+        upStairButton.setImageResource(R.mipmap.up_stairs);
     }
 }
